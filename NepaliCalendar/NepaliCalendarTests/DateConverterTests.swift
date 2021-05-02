e77e885c39b187f49e00c28a7003bbfa6846a153
alok <alksbd9@gmail.com>
2021-05-02 22:47:48 +0545

    Use test specific computed property for better readability

--- NepaliCalendar/NepaliCalendarTests/DateConverterTests.swift
+++ NepaliCalendar/NepaliCalendarTests/DateConverterTests.swift
@@ -14,26 +14,26 @@ class DateConverterTests: XCTestCase {
         let datesLessThanOne = [(0, 0), (-1, -1), (-10, -10), (1, -1), (-1, 2)]
         
         try? datesLessThanOne.forEach { (day, month) in
-            XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: day, month: month, year: 2078)))
+            XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: day, month: month, year: anyValidYear)))
         }
     }
     
     func test_bSToAd_throwsErrorWhenMonthIsGreaterThan12() {
         let monthsGreaterThan12 = [13, 24, 100]
         
-        try? monthsGreaterThan12.forEach { XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: 1, month: $0, year: 0))) }
+        try? monthsGreaterThan12.forEach { XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: anyValidDay, month: $0, year: anyValidYear))) }
     }
     
     func test_bSToAd_throwsErrorWhenDayIsGreaterThan32() {
         let daysGreaterThan32 = [33, 40, 100]
         
-        try? daysGreaterThan32.forEach { XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: $0, month: 1, year: 0))) }
+        try? daysGreaterThan32.forEach { XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: $0, month: anyValidMonth, year: anyValidYear))) }
     }
     
     func test_bSToAd_throwsErrorWhenYearIsNotInRange() {
         let invalidYears = [1, 100, 5000, 100000, 1999, 2091]
         
-        try? invalidYears.forEach { XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: 1, month: 1, year: $0))) }
+        try? invalidYears.forEach { XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: anyValidDay, month: anyValidMonth, year: $0))) }
     }
     
     func test_bSToAd_throwsErrorWhenDayIsInvalidForGivenMonthOfYear() {
@@ -42,6 +42,20 @@ class DateConverterTests: XCTestCase {
         try? invalidDaysForMonthOfYear.forEach { (day, month, year) in
             XCTAssertThrowsError(try DateConverter.bSToAD(date: NCDate(day: day, month: month, year: year)))
         }
+    }
+    
+    //MARK: Helpers
+    
+    private var anyValidDay: Int {
+        1
+    }
+    
+    private var anyValidMonth: Int {
+        1
+    }
+    
+    private var anyValidYear: Int {
+        2078
     }
     
 }
